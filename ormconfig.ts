7c5b30701094f35bef737e243b5a4a5d7237dd67
Cintia Masumi Nomura <nomura_masumi@hotmail.com>
2024-07-26 12:04:04 -0300

    fix : set busy timeout in dataSource

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,24 +5,22 @@ export const dataSourceOptions: DataSourceOptions = {
     "database": "src/database/database.sqlite",
     "entities": ["dist/**/*.entity{.ts,.js}"],
     "synchronize": true,
-    "migrations":["dist/database/migrations/*.{.ts.js}"]
+    "migrations":["dist/database/migrations/*.{.ts.js}"],
+    "extra": {
+        "connectionLimit": 1,
+    }
 }
 
 export const dataSource = new DataSource(dataSourceOptions)
 
 dataSource.initialize()
     .then(() => {
-          
-          dataSource.query('PRAGMA busy_timeout = 60000')
-          .then(() => {
-              console.log('PRAGMA busy_timeout set to 60000');
-          })
-          .catch((err) => {
-              console.error('Error setting PRAGMA busy_timeout', err);
-          });
-        console.log('Data Source initialize')
-    }
-    ).catch ((err) => {
-        console.error('Error initialize Data Source', err)
+        // Configurar PRAGMA após a inicialização do DataSource
+        return dataSource.query('PRAGMA busy_timeout = 60000');
     })
-
+    .then(() => {
+        console.log('PRAGMA busy_timeout set to 60000');
+    })
+    .catch((err) => {
+        console.error('Error initializing Data Source or setting PRAGMA busy_timeout', err);
+    })
