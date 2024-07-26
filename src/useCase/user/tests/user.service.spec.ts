c1ea498a63b0de6717df5e5a04d94a2d70893033
Cintia Masumi Nomura <nomura_masumi@hotmail.com>
2024-07-26 12:17:03 -0300

    test : fix UserService  update user

--- src/useCase/user/tests/user.service.spec.ts
+++ src/useCase/user/tests/user.service.spec.ts
@@ -1,59 +1,13 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { User } from 'src/database/entities/user/user.entity';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import * as bcrypt from 'bcryptjs';
-import { UserService } from '../service/user.service';
-import { mockOneUser, mockUser, mockUserRepository, mockUserTree } from 'src/__mock__/userMock';
+import { Test, TestingModule } from '@nestjs/testing'
+import { User } from 'src/database/entities/user/user.entity'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { UserService } from '../service/user.service'
+import { mockOneUser, mockUser, mockUserRepository, mockUserTree } from 'src/__mock__/userMock'
 
 describe('UserService', () => {
-    let service: UserService;
-    let repository: Repository<User>;
-  
-    // Mock de um usuários
-    /*const mockUser = [
-      {
-        id: 'b72add7e-5a4b-42fb-b988-4fb0a2555439',
-        userName: 'testName',
-        parentUserId: null
-      },
-      {
-        id: 'c41aeb9b-3370-44a2-9b68-590c53210e88',
-        userName: 'testName2',
-        parentUserId: 'b72add7e-5a4b-42fb-b988-4fb0a2555439'
-      }
-    ];
-
-    const mockUserTree = [
-      {
-        id: 'b72add7e-5a4b-42fb-b988-4fb0a2555439',
-        userName: 'testName',
-        parentUserId: null,
-        parents: [
-          {
-            id: 'c41aeb9b-3370-44a2-9b68-590c53210e88',
-            userName: 'testName2',
-            parentUserId: 'b72add7e-5a4b-42fb-b988-4fb0a2555439',
-            parents:[]
-          }
-        ]
-      } 
-    ]
-
-    const mockOneUser = {
-      id: 'b72add7e-5a4b-42fb-b988-4fb0a2555439',
-      userName: 'testName',
-      parentUserId: null
-    }
-  
-    const mockUserRepository = {
-      find: jest.fn().mockResolvedValue(mockUser),
-      findOneBy: jest.fn().mockResolvedValue(mockOneUser),
-      findOneById: jest.fn().mockResolvedValue(mockOneUser),
-      update: jest.fn().mockResolvedValue(undefined),
-      save: jest.fn().mockResolvedValue(mockUser),
-      remove: jest.fn().mockResolvedValue(undefined),
-    };*/
+    let service: UserService
+    let repository: Repository<User>
   
     beforeEach(async () => {
       const module: TestingModule = await Test.createTestingModule({
@@ -64,23 +18,24 @@ describe('UserService', () => {
             useValue: mockUserRepository,
           },
         ],
-      }).compile();
+      }).compile()
   
       service = module.get<UserService>(UserService);
       repository = module.get<Repository<User>>(getRepositoryToken(User));
     });
   
     it('should be defined', () => {
-      expect(service).toBeDefined();
-    });
+      expect(service).toBeDefined()
+    })
   
     describe('getUsersTree', () => {
       it('should return a user tree', async () => {
-        const result = await service.getUsersTree();
-        expect(result).toEqual(mockUserTree);
-        expect(repository.find).toHaveBeenCalledWith({ where: { isDeleted: false } });
-      });
-    });
+        const result = await service.getUsersTree()
+        expect(result).toEqual(mockUserTree)
+        expect(repository.find).toHaveBeenCalledWith({ where: { isDeleted: false } })
+      })
+
+    })
   
     describe('findOneById', () => {
       it('should return a user by ID', async () => {
@@ -88,24 +43,37 @@ describe('UserService', () => {
         
         expect(result).toEqual(mockOneUser);
         expect(repository.findOneBy).toHaveBeenCalledWith({ id: mockUser[0].id });
-      });
-    });
+      })
+
+    })
   
     describe('update', () => {
       it('should update a user', async () => {
-        const userUpdate = { userName: 'updatedName', passWord: 'newPass' };
-        const hashedPassword = 'hashedPassword';
+        const mockOneUser = { id: 'b72add7e-5a4b-42fb-b988-4fb0a2555439' };
+        const userUpdate = {
+          userName: 'updatedName',
+          passWord: 'hashedPassword',
+        };
+        const hashedPassword = 'hashedPassword'; 
+
         jest.spyOn(service, 'updatePassword').mockResolvedValue(hashedPassword);
         await service.update(mockOneUser.id, userUpdate);
-        expect(repository.update).toHaveBeenCalledWith(mockOneUser.id, { ...userUpdate, passWord: hashedPassword });
-      });
-    });
+
+        expect(repository.update).toHaveBeenCalledWith(mockOneUser.id, {
+          userName: userUpdate.userName,
+          passWord: hashedPassword,
+        })
+
+      })
+
+    })
   
     describe('remove', () => {
       it('should mark a user as deleted', async () => {
         await service.remove(mockOneUser.id);
         expect(repository.save).toHaveBeenCalledWith({ ...mockOneUser, isDeleted: true });
-      });
-    });
+      })
 
-  });
+    })
+
+  })
